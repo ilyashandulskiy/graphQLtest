e44443c6171a0e346ddf3e8b1e0aad433b7265c9
Ilya <ilyashandulskiy@gmail.com>
2022-03-03 23:12:23 +0500

    Refactoring: query fix

--- src/query/continent.ts
+++ src/query/continent.ts
@@ -8,5 +8,4 @@ export const GET_CONTINETS = gql`
     }
   }
 
-
 `
--- src/query/country.ts
+++ src/query/country.ts
@@ -1,7 +1,6 @@
 import { gql } from '@apollo/client'
 
 export const GET_COUNTRIES = ({id} : {id: any}) => gql`
-# Write your query or mutation here
   query {
     continent(code: "${id}") {
       name
@@ -15,19 +14,18 @@ export const GET_COUNTRIES = ({id} : {id: any}) => gql`
 `
 
 export const GET_COUNTRY = ({id} : {id: any}) => gql`
-# Write your query or mutation here
   query {
     country(code: "${id}") {
-    name
-    code
-    native
-    capital
-    currency
-    languages {
       name
       code
+      native
+      capital
+      currency
+      languages {
+        name
+        code
+      }
     }
-  }
   }
 
 `
